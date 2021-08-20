030cb8e694892121e6f3f6355f08a9c8b3880ef6
yunchipang <yunchipang@gmail.com>
2021-08-20 16:34:06 +0800

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,20 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Yunchi PANG
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
+
+    /** Check if the year arg is a Leap Year */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
+    }
+
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
